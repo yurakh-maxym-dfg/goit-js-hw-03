7f45395e6edadd4a5bc2d326239f95db27afab74
yurakh-maxym-dfg <maksaaasssdfg@gmail.com>
2026-03-09 13:13:31 +0200

    task-3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function filterArray(numbers, value) {
+    let newArray = [];
+    for (let item of numbers){
+        if (item > value){
+            newArray.push(item);
+        }
+    }
+    return newArray;
+}
+
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
